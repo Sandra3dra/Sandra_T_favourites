214106c9855b029ad1d50c0f00b3917124604e3c
SandraTsao <43250383+SandraTsao@users.noreply.github.com>
2019-12-05 02:01:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    host: "localhost",
-    port: 8889, // 8889 for mac, 3306 for win
-    uname: "root",
-    pword: "root", // leave blank for windows
-    database: "db_fav"
+    host: "us-cdbr-iron-east-05.cleardb.net",
+    port: 3306, // 8889 for mac, 3306 for win
+    uname: "b2404466e3b3bd",
+    pword: "b50dd0d6", // leave blank for windows
+    database: "heroku_668f38aad7785d7"
 }
 
 module.exports = config;
